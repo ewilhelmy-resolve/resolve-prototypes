ed6f257571075c0b5995c0d3ef0daa1c02d64872
Andres Araujo <a.araujo.azua@gmail.com>
2025-09-22 13:41:36 -0600

    chore: Clean up Files navbar menu and remove tanstack devtools

--- packages/client/src/providers/QueryProvider.tsx
+++ packages/client/src/providers/QueryProvider.tsx
@@ -1,5 +1,5 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+// import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { useState } from 'react'
 
 interface QueryProviderProps {
@@ -37,7 +37,7 @@ export function QueryProvider({ children }: QueryProviderProps) {
   return (
     <QueryClientProvider client={queryClient}>
       {children}
-      <ReactQueryDevtools initialIsOpen={false} />
+      {/*<ReactQueryDevtools initialIsOpen={false} />*/}
     </QueryClientProvider>
   )
 }
