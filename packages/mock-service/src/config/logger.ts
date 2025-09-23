7a394cfbed06c41bf35e579099696c1eaf4b2a71
Andres Araujo <a.araujo.azua@gmail.com>
2025-09-22 22:59:28 -0600

    Fix lint issues

--- packages/mock-service/src/config/logger.ts
+++ packages/mock-service/src/config/logger.ts
@@ -99,6 +99,8 @@ export const createContextLogger = (
     tenantId?: string;
     conversationId?: string;
     documentId?: string;
+    email?: string;
+    pendingUserId?: string;
   }
 ) => {
   return baseLogger.child({
