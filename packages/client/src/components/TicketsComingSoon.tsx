d61efed811848cf2718e256a4e79d65dee913a35
Edgar Moreno <edgar.moreno@resolve.io>
2025-10-22 17:39:47 -0600

    moved title

--- packages/client/src/components/TicketsComingSoon.tsx
+++ packages/client/src/components/TicketsComingSoon.tsx
@@ -1,15 +1,17 @@
 export default function TicketsComingSoon() {
 	return (
 		<div className="flex flex-col items-center gap-6 w-full max-w-[1120px] mx-auto">
-			<h1 className="text-primary text-center text-[29px] font-[420] leading-7">
-				Tickets coming soon
-			</h1>
-
+	
 			<img
 				src="/images/ticketcomingsoon.png"
 				className="w-[338px] h-[231px] object-fill"
 				alt="Tickets coming soon"
 			/>
+
+      <h1 className="text-primary text-center text-[29px] font-[420] leading-7">
+				Tickets coming soon
+			</h1>
+
 
 			<p className="text-center text-base font-normal leading-6 max-w-2xl">
 				<p>Leverage your existing ITSM data for intelligent analysis.</p>
